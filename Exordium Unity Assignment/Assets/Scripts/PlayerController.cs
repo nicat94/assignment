6a0184ff6755468399ac6ab1097e5ad3f73eac18
Nijat Nasibli <n.nasibli2@gmail.com>
2018-08-23 13:30:21 +0400

    first movement

---
+++ Exordium Unity Assignment/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float Speed;
+
+    private Rigidbody2D _rigidBody;
+
+    
+    void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody2D> ();
+    }
+
+    
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+
+        _rigidBody.velocity = movement * Speed;
+        
+    }
+}
